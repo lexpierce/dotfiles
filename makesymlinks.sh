b05661a3a3e5dad71c067d2440298bb1918f64d9
Alexander Pierce <alex@lexpierce.com>
2020-01-12 17:30:37 -0800

    Adding $XDK_CONFIG_HOME directories

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles
+# in ${HOME}/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles/old             # old dotfiles backup directory
-files="$(ls ~/dotfiles | grep -v makesymlinks | grep -vw old)"
+dotfiles=${HOME}/dotfiles               # dotfiles directory
+olddir=${HOME}/dotfiles/old             # old dotfiles backup directory
+files="$(ls ${HOME}/dotfiles | grep -v makesymlinks | grep -vw old | grep -v xdg)"
+xdg_config="kitty git"
 
 ##########
 
-# create dotfiles_old in ~ if needed...
-[[ -d $olddir ]] || {
-  echo -n "Creating $olddir for backup of any existing dotfiles in ~"
-  mkdir -p $olddir
+# create dotfiles_old in ${HOME} if needed...
+[[ -d ${olddir} ]] || {
+  echo -n "Creating ${olddir} for backup of any existing dotfiles in ${HOME}"
+  mkdir -p ${olddir}
 }
 
-git clone https://github.com/chriskempson/base16-shell.git ${HOME}/.config/base16-shell
+[[ -d ${HOME}/.config/base16-shell ]] &&
+  git clone https://github.com/chriskempson/base16-shell.git \
+   ${HOME}/.config/base16-shell
 
+cd ${dotfiles} || exit
 
-cd $dir || exit
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-echo "Moving any existing dotfiles from ~ to $olddir and creating symlinks."
-for file in $files; do
-  [[ -h ~/.$file ]] || {
-    [[ -f ~/.$file || -d ~/.$file ]] && {
-      echo "Moving .$file to ${olddir}."
-      mv ~/".$file" "$olddir"/"$file"
+# move any existing dotfiles in homedir to dotfiles_old directory, then create
+#symlinks from the homedir to any files in the ${HOME}/dotfiles directory specified in ${file}s
+echo "Moving any existing dotfiles from ${HOME}/ to ${olddir} and creating symlinks."
+for file in ${file}s; do
+  [[ -h ${HOME}/.${file} ]] || {
+    [[ -f ${HOME}/.${file} || -d ${HOME}/.${file} ]] && {
+      echo "Moving .${file} to ${olddir}."
+      mv ${HOME}/".${file}" "${olddir}"/"${file}"
     }
-    echo "Creating symlink to $file in home directory."
-    ln -s "$dir"/"$file" ~/".$file"
+    echo "Creating symlink to ${dotfiles}/${file} in home directory."
+    ln -s "$dir"/"${file}" ${HOME}/".${file}"
   }
+done
+echo "Moving any existing files or directories from ${XDG_CONFIG_HOME:-"${HOME}/.config"} to ${olddir} and creating symlinks."
+for dir in ${xdg_config}; do
+  [[ -h ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir} ]] ||
+    [[ -f ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir} || -d ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir} ]] && {
+      echo "Moving ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir} to ${olddir}."
+      mv ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir} ${olddir}/${dir}
+  }
+  echo "Creating symlink to ${dotfiles}/xdg/${dir} in ${XDG_CONFIG_HOME:-${HOME}/.config}"
+  ln -s "${dotfiles}/xdg/${dir}" ${XDG_CONFIG_HOME:-${HOME}/.config}/${dir}
 done
 
 # Install zimfw if not yet done
-[[ -f ~/.zim/zimfw.zsh ]] || {
-  mkdir ~/.zim
-  curl -L -o ~/.zim/zimfw.zsh https://raw.githubusercontent.com/zimfw/zimfw/master/zimfw.zsh
-  cd ~/.zim
-  echo "run source ~/.zim/zimfw.zsh install"
+[[ -f ${HOME}/.zim/zimfw.zsh ]] || {
+  mkdir ${HOME}/.zim
+  curl -L -o ${HOME}/.zim/zimfw.zsh https://raw.githubusercontent.com/zimfw/zimfw/master/zimfw.zsh
+  cd ${HOME}/.zim
+  echo "run source ${HOME}/.zim/zimfw.zsh install"
 }
