6a4fb22be4612f75568d8cbabab4894e6f6a3eb5
Alexander J Pierce <alex@lexpierce.com>
2023-07-12 14:41:06 -0700

    indentation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
     if command -sq zoxide
         zoxide init --hook pwd fish | source
