5f5805869e8e3d51a0e98d9e8d420e1f68c2c457
Alexander J Pierce <alex@lexpierce.com>
2023-11-27 22:50:06 -0800

    update some fish configs

---
+++ .config/fish/conf.d/homebrew.fish
@@ -0,0 +1,3 @@
+if command -sq /opt/homebrew/bin/brew
+	eval (/opt/homebrew/bin/brew shellenv fish)
+end
--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -1,4 +1,8 @@
 if not status is-interactive && test "$CI" != true
     exit
 end
-starship init fish | source
+if command -sq starship
+	starship init fish | source
+else
+	echo 'starship: command not found, please install it from https://starship.rs'
+end
