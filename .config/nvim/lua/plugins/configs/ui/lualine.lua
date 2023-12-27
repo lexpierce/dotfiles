72d65eae00586b3dc055abba761f595ce4e1af3d
Alexander J Pierce <alex@lexpierce.com>
2023-12-27 09:48:07 -0800

    Comment out defaults in lualine options

--- .config/nvim/lua/plugins/configs/ui/lualine.lua
+++ .config/nvim/lua/plugins/configs/ui/lualine.lua
@@ -6,7 +6,7 @@ return {
 		section_separators = { left = '', right = ''},
 		--disabled_filetypes = {
 		--	statusline = {},
-		--	winbar = {},
+		--	winbar = {}
 		--},
 		--ignore_focus = {},
 		always_divide_middle = false,
@@ -14,27 +14,28 @@ return {
 		--refresh = {
 		--	statusline = 1000,
 		--	tabline = 1000,
-		--	winbar = 1000,
+		--	winbar = 1000
+		--}
 	},
 	sections = {
-		lualine_a = {'mode'},
-		--lualine_b = {'branch', 'diff', 'diagnostics'},
+	--	lualine_a = {'mode'},
+	--	lualine_b = {'branch', 'diff', 'diagnostics'},
 		lualine_b = {{'b:gitsigns_head', icon = '', icons_enabled = true}},
-		lualine_c = {'filename'},
-		lualine_x = {'encoding', 'fileformat', 'filetype'},
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
+	--	lualine_c = {'filename'},
+	--	lualine_x = {'encoding', 'fileformat', 'filetype'},
+	--	lualine_y = {'progress'},
+	--	lualine_z = {'location'}
 	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
-		lualine_y = {},
-		lualine_z = {}
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
+	--inactive_sections = {
+	--	lualine_a = {},
+	--	lualine_b = {},
+	--	lualine_c = {'filename'},
+	--	lualine_x = {'location'},
+	--	lualine_y = {},
+	--	lualine_z = {}
+	--},
+	--tabline = {},
+	--winbar = {},
+	--inactive_winbar = {},
 	extensions = {'lazy'}
 }
