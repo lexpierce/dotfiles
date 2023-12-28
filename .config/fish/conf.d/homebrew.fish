0f1b406ab3c619fd1204219d2a0acd4446c8cfdc
Alexander J Pierce <alex@lexpierce.com>
2023-12-27 16:08:34 -0800

    move path overrides for homebrew components to homebrew config file

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,3 +1,9 @@
 if command -sq /opt/homebrew/bin/brew
 	eval (/opt/homebrew/bin/brew shellenv fish)
 end
+
+# Homebrew version overrides
+
+set -xpg PATH /opt/homebrew/opt/openjdk@17/bin
+set -xpg PATH /opt/homebrew/opt/node@18/bin
+set -xpg PATH /opt/homebrew/opt/python@3.12/libexec/bin
