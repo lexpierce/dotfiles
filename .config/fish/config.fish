1e46ecd3eea44bc833411967b3cd5166d7c22860
Alexander J Pierce <alex@lexpierce.com>
2024-01-03 14:23:04 -0800

    stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,13 @@ if status is-interactive
 	else
 		echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
 	end
-	# FZF color & same defaults from PatrickF1/fzf.fish
+	# FZF color & sane defaults from PatrickF1/fzf.fish
 	set -xg FZF_DEFAULT_OPTS "--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker='*' \
 	--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 	--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 	--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+
+	# iTerm2 shell integration
+	test -e ~/.iterm2_shell_integration.fish ; and source ~/.iterm2_shell_integration.fish ; or true
 end
+
